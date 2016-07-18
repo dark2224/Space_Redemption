7d513c9e84b00aea9928a97447000d10315c1b54
yunu95 <liunux95@gmail.com>
2016-07-18 16:36:11 +0900

    Pending edit
    이상한 경고가 뜨는게 보기 싫어서 코밑해봤습니다.

---
+++ Source/Space_Redemption/InteractionStatus.h
@@ -0,0 +1,5 @@
+#pragma once
+
+enum InteractionStatus{
+Idle, Approaching, Interacting
+};
--- Source/Space_Redemption/TangibleActor.h
+++ Source/Space_Redemption/TangibleActor.h
@@ -4,8 +4,8 @@
 #pragma once
 
 #include "GameFramework/Actor.h"
+#include "InteractionStatus.h"
 #include "TangibleActor.generated.h"
-
 UCLASS()
 class SPACE_REDEMPTION_API ATangibleActor : public AActor
 {
@@ -14,10 +14,11 @@ protected:
 	// 메쉬에 대한 손의 상대적 위치와 각도를 저장합니다. Set메서드를 통해 블루프린트상에서 초기화해줘야 합니다.
 	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
 		class USceneComponent *DesiredHandTransform;
+	class UHand* TargetHand;
 	// 손이 오브젝트에 다가갈 때 호출되는 함수입니다. 호출되었을 때 손이 상호작용을 시작하지는 않지만
 	// 손이 오브젝트에 가까워짐에 따라 애니메이션이 상호작용 idle 애니메이션으로 블렌딩됩니다.
 	UFUNCTION(BlueprintCallable, Category = "Updatingstat")
-		virtual void Beingapproached(class UHand* param) {}
+		virtual void Beingapproached(class UHand* param) { TargetHand = param; }
 	// 손과 오브젝트가 상호작용을 시작해야 할때 호출됩니다. 이때부터 손과 오브젝트는 서로 딱 붙게 됩니다.
 	UFUNCTION(BlueprintCallable, Category = "Updatingstat")
 		virtual void StartInteraction(class UHand* param) {}
@@ -27,6 +28,7 @@ protected:
 	// 손 위치에 대한 세터
 	UFUNCTION(BlueprintCallable, Category = "InitializationRequired")
 		void SetDesiredHandTransform(USceneComponent* param) { DesiredHandTransform = param; }
+
 public:
 	// Sets default values for this actor's properties
 	ATangibleActor();
