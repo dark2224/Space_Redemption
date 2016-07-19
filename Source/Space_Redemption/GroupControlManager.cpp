9690cf7d814ab6b90ad16ea48238e331d6802996
Pyhton <dark2224@naver.com>
2016-07-19 09:54:39 +0900

    AI Test중

--- Source/Space_Redemption/GroupControlManager.cpp
+++ Source/Space_Redemption/GroupControlManager.cpp
@@ -1,40 +1,35 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Space_Redemption.h"
-#include "TangibleActor.h"
+#include "GroupControlManager.h"
 
+#include <algorithm>
 
 // Sets default values
-ATangibleActor::ATangibleActor()
+AGroupControlManager::AGroupControlManager()
+	: m_GroupName(NULL)
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+}
 
-}
-void ATangibleActor::Beingapproached(class UHand* param)
-{
-	
-}
 // Called when the game starts or when spawned
-void ATangibleActor::BeginPlay()
+void AGroupControlManager::BeginPlay()
 {
-	Super::BeginPlay();
-	
+	Super::BeginPlay();	
 }
 
 // Called every frame
-void ATangibleActor::Tick( float DeltaTime )
+void AGroupControlManager::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
-
 }
 
-void ATangibleActor::StartInteraction(class UHand* param)
+void AGroupControlManager::SetGroup_Actor(class AActor* pActor)
 {
-
 }
 
-void ATangibleActor::QuitInteraction(class UHand* param)
+std::list<class AActor*>* AGroupControlManager::Get_ActorList(const TCHAR* pGroupName)
 {
-
+	return NULL;
 }
