06fc8e20ad626115a33a7e23a27374186fd311e3
yunu95 <liunux95@gmail.com>
2016-07-28 17:06:19 +0900

    내부기계부품들 더욱 다듬음

--- Source/Space_Redemption/MiniGun.cpp
+++ Source/Space_Redemption/MiniGun.cpp
@@ -2,36 +2,54 @@
 
 #include "Space_Redemption.h"
 #include "TouchPad.h"
+#include <cmath>
 #include "MiniGun.h"
 
 
 // Sets default values
 AMiniGun::AMiniGun()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	RootComponent = CreateDefaultSubobject<USceneComponent>("RootComponent");
 	LaserPointer = CreateDefaultSubobject<UStaticMeshComponent>("LaserPointer");
 	RealGunMesh = CreateDefaultSubobject<UStaticMeshComponent>("RealGun");
+	LaserPointer->AttachToComponent(RootComponent,FAttachmentTransformRules::KeepRelativeTransform);
+	RealGunMesh->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
 }
 
 // Called when the game starts or when spawned
 void AMiniGun::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 // Called every frame
-void AMiniGun::Tick( float DeltaTime )
+void AMiniGun::Tick(float DeltaTime)
 {
-	Super::Tick( DeltaTime );
+	Super::Tick(DeltaTime);
 	if (targetPad) {
-		LaserPointer->SetRelativeRotation(FRotator(Clamper.Pitch*(targetPad->GetMappingCoordinate().X+1),Clamper.Yaw*targetPad->GetMappingCoordinate().Y , 0));
+		LaserPointer->SetRelativeRotation(FRotator(Clamper.Pitch*(targetPad->GetMappingCoordinate().X + 1), Clamper.Yaw*targetPad->GetMappingCoordinate().Y, 0));
 	}
-	else {
-		
+	//레이저 포인터와 총구가 향하는 방향이 같지 않은가?
+	if (!LaserPointer->RelativeRotation.Equals(RealGunMesh->RelativeRotation))
+	{
+		//그 방향간의 변위가 졸라 작은가?
+		if (SquaredRotatorSize(LaserPointer->RelativeRotation - RealGunMesh->RelativeRotation) < powf(GunRotatingSpeed*DeltaTime, 2))
+			RealGunMesh->SetRelativeRotation(LaserPointer->RelativeRotation);
+		else
+		{
+			RealGunMesh->AddLocalRotation(DeltaTime*GunRotatingSpeed / RotatorSize(LaserPointer->RelativeRotation - RealGunMesh->RelativeRotation)*(LaserPointer->RelativeRotation - RealGunMesh->RelativeRotation));
+		}
 	}
 }
 void AMiniGun::LinkPad(ATouchPad* target) {
 	targetPad = target;
+}
+float AMiniGun::RotatorSize(FRotator param) {
+	return sqrtf(param.Pitch*param.Pitch + param.Roll*param.Roll + param.Yaw*param.Yaw);
+}
+float AMiniGun::SquaredRotatorSize(FRotator param) {
+	return param.Pitch*param.Pitch + param.Roll*param.Roll + param.Yaw*param.Yaw;
 }
--- Source/Space_Redemption/MiniGun.h
+++ Source/Space_Redemption/MiniGun.h
@@ -10,20 +10,22 @@ class SPACE_REDEMPTION_API AMiniGun : public AActor
 {
 	GENERATED_BODY()
 private:
+	const float GunRotatingSpeed = 360;
 	const FRotator Clamper = FRotator(22.5f,70.0f,0);
 	class ATouchPad* targetPad;
+	float RotatorSize(struct FRotator param);
+	float SquaredRotatorSize(struct FRotator param);
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
-	UMeshComponent* LaserPointer;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Mesh")
-	UMeshComponent* RealGunMesh;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Mesh")
+	UStaticMeshComponent* LaserPointer;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Mesh")
+	UStaticMeshComponent* RealGunMesh;
 public:	
 	// Sets default values for this actor's properties
 	AMiniGun();
 
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
 	// Called every frame
 	virtual void Tick( float DeltaSeconds ) override;
 	void LinkPad(class ATouchPad* target);
